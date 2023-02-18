70ad64170923adbbd7b46fc4b4e98f0cbb0fab86
avnish kumar <11avnishkumar@gmail.com>
2023-02-18 14:53:57 +0530

    updated

--- git-automation.cpp
+++ git-automation.cpp
@@ -8,11 +8,8 @@ using std::string;
 int main(){
 char userInput;
 string message,status;
-cout<<"Note:\nThe program only update the existing repository.The program is vulnerable to the underlaying operaing system and it should not be used for security reason."<<endl;
-cout<<"***************************************************"<<endl;
-cout<<"The git command started..."<<endl;
+cout<<"Welcome to the git automation program."<<endl;
 system("date");
-//system("git status");
 // Check if branch is up to date with remote
 system("git status > status.txt"); // redirect output to file
 std::ifstream statusFile("status.txt");
@@ -23,11 +20,12 @@ if (status.find("Your branch is up to date with 'origin'") != std::string::npos)
     return 0;
 }
 system("rm status.txt"); // delete the temporary status file
-system("git add -A");
 cout<<"Do you want to proceed further? Y or N: ";
 cin>>userInput;
 cin.ignore();
 if(userInput == 'y' || userInput == 'Y'){
+system("git status");
+system("git add -A");
 cout<<"Enter the commit message:"<<endl;
 getline(cin,message);
 string command = "git commit -m \""+message+"\"";
